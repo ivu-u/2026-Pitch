dd5ca300707b1b44f32ffff783cf1039551d77dd
Thomas Vu <139417064+ThomasNVu@users.noreply.github.com>
2026-01-27 15:57:49 -0500

    Integrate FMOD into Unity

--- Dust-Bunnies/Assets/_Scripts/Audio/Core/AudioEvents.cs
+++ Dust-Bunnies/Assets/_Scripts/Audio/Core/AudioEvents.cs
@@ -1,25 +1,25 @@
-using UnityEngine;
-using FMODUnity;
+// using UnityEngine;
+// using FMODUnity;
 
-[CreateAssetMenu(menuName = "Audio/Audio Events")]
-public class AudioEvents : ScriptableObject
-{
-    [Header("UI")]
-    // public EventReference uiClick;
+// [CreateAssetMenu(menuName = "Audio/Audio Events")]
+// public class AudioEvents : ScriptableObject
+// {
+//     [Header("UI")]
+//     // public EventReference uiClick;
 
-    [Header("Player - Movement")]
-    public EventReference playerFootstep;
+//     [Header("Player - Movement")]
+//     public EventReference playerFootstep;
 
-    [Header("Music")]
-    // public EventReference music;
+//     [Header("Music")]
+//     // public EventReference music;
 
-    [Header("Ambience")]
-    // public EventReference ambient;
+//     [Header("Ambience")]
+//     // public EventReference ambient;
 
-    [Header("Environment")]
-    public EventReference pageFlip;
-    public EventReference plasticTap;
-    public EventReference plushySqueeze;
-    public EventReference pencilPickup;
-    public EventReference pencilDrop;
-}
+//     [Header("Environment")]
+//     public EventReference pageFlip;
+//     public EventReference plasticTap;
+//     public EventReference plushySqueeze;
+//     public EventReference pencilPickup;
+//     public EventReference pencilDrop;
+// }
